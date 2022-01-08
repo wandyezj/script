87e410bf843ccf8d173aa6e74a4398b280d9e691
wandyezj <wandyezj@gmail.com>
2022-01-07 18:54:49 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
+# make and run
+# may need to run dos2unix if get errors
+# sudo apt install dos2unix
+clear
 make
-if [ $? -eq 0 ];then
+if [ $? -eq 0 ];
+then
     cd bin
     echo
-    ./script
+    ./script test/basic.test.txt
     echo
     cd ..
 fi
