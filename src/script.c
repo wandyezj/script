f04c25748cb04cc2e97bec61ec659fc42e039719
wandyezj <wandyezj@gmail.com>
2022-01-07 19:42:36 -0800

    Update script.c

--- src/script.c
+++ src/script.c
@@ -37,26 +37,11 @@ bool is_newline(char c) {
     return c == '\n';
 }
 
-int main(int argc, char **argv) 
-{
-    printf("%i\n", argc);
-    printf("%s\n", argv[0]);
-    printf("Hello World!\n");
+bool is_hash(char c) {
+    return c == '#';
+}
 
-    if (argc != 2) {
-        // should pass in the file path
-        printf("usage: script file");
-        return 1;
-    }
-
-    char* file_path = argv[1];
-
-    // read in file and print it out
-
-    // https://stackoverflow.com/questions/3463426/in-c-how-should-i-read-a-text-file-and-print-all-strings#:~:text=The%20simplest%20way%20is%20to%20read%20a%20character%2C,number%2C%20and%20a%20plain%20char%20may%20be%20unsigned.
-
-    // inefficent to read individual characters but this is simple enough for now.
-
+void detect_characters_in_file(char* file_path) {
     int c;
     FILE *file;
     file = fopen(file_path, "r");
@@ -83,13 +68,116 @@ int main(int argc, char **argv)
             if (is_newline(c)) {
                 printf("(newline)");
             }
+
+            if (is_hash(c)) {
+                printf("(hash)");
+            }
         }
-            
+
         fclose(file);
     } else {
         printf("failed to open file [%s]", file_path);
     }
+}
 
+enum character_type {
+    hash,
+    space,
+    newline,
+
+
+    unknown,
+};
+
+enum character_type get_character_type(char c) {
+    if (is_newline(c)) {
+        return newline;
+    }
+
+    if (is_hash(c)) {
+        return hash;
+    }
+
+
+    return unknown;
+}
+
+int execute_script_file(char* file_path) {
+    int c;
+    FILE *file;
+    file = fopen(file_path, "r");
+    if (!file) {
+        printf("failed to open file [%s]", file_path);
+        return 1;
+    }
+
+    bool inside_comment = false;
+    while ((c = getc(file)) != EOF) {
+
+        enum character_type t = get_character_type(c);
+
+        switch (t) {
+            case hash:
+                inside_comment = true;
+                break;
+            case newline:
+                inside_comment = false;
+                break;
+            default:
+                if (!inside_comment) {
+                    printf("%c", c);
+                }
+                
+                break;
+        }
+
+        // if (is_newline(c) ) {
+        //     // end any single line comment
+        //     if(inside_comment) {
+        //         inside_comment = false;
+        //     }
+
+        // } else if (is_hash(c)) {
+        //     inside_comment = true;
+        // }
+
+        // if (!inside_comment) {
+        //     // can do some other things
+        //     printf("%c", c);
+        // }
+
+    }
 
     return 0;
+}
+
+
+int main(int argc, char **argv) 
+{
+    //printf("%i\n", argc);
+    //printf("%s\n", argv[0]);
+    //printf("Hello World!\n");
+
+    if (argc != 2) {
+        // should pass in the file path
+        printf("usage: script file");
+        return 1;
+    }
+    
+    char* file_path = argv[1];
+
+    // read in file and print it out
+
+    // https://stackoverflow.com/questions/3463426/in-c-how-should-i-read-a-text-file-and-print-all-strings#:~:text=The%20simplest%20way%20is%20to%20read%20a%20character%2C,number%2C%20and%20a%20plain%20char%20may%20be%20unsigned.
+
+    // inefficent to read individual characters but this is simple enough for now.
+
+    // detect_characters_in_file(file_path);
+
+
+    printf("-----\n");
+    // place output between two bars
+    int result = execute_script_file(file_path);
+    printf("\n-----\n");
+    return result;
 }
